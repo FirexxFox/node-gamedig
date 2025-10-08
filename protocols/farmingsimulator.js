1e6ca572f2529f98acec0321052e4cd6fc8468f7
FireFox <czarnapantera619@gmail.com>
2025-10-08 16:20:03 +0200

    Update farmingsimulator.js

--- protocols/farmingsimulator.js
+++ protocols/farmingsimulator.js
@@ -2,68 +2,99 @@ import Core from './core.js'
 import { XMLParser, XMLValidator } from 'fast-xml-parser'
 
 export default class farmingsimulator extends Core {
-  async run (state) {
+  async run(state) {
     if (!this.options.port) this.options.port = 8080
-    if (!this.options.token) throw new Error(`No token provided. You can get it from http://${this.options.host}:${this.options.port}/settings.html`)
+    if (!this.options.token)
+      throw new Error(`No token provided. You can get it from http://${this.options.host}:${this.options.port}/settings.html`)
 
-    const request = await this.request({
-      url: `http://${this.options.host}:${this.options.port}/feed/dedicated-server-stats.xml?code=${this.options.token}`,
-      responseType: 'text'
-    })
-
-    const isValidXML = XMLValidator.validate(request)
-    if (!isValidXML) {
-      throw new Error('Invalid XML received from Farming Simulator Server')
-    }
-
-    const parser = new XMLParser({ ignoreAttributes: false })
-    const parsed = parser.parse(request)
-
-    const serverInfo = parsed.Server
-    const playerInfo = serverInfo.Slots
-
-    // Attributes in fast-xml-parser are prefixed with @_
-
-    state.name = serverInfo['@_name']
-    state.map = serverInfo['@_mapName']
-    state.numplayers = parseInt(playerInfo['@_numUsed'], 10) || 0
-    state.maxplayers = parseInt(playerInfo['@_capacity'], 10) || 0
-
-    const players = playerInfo.Player
-
-    for (const player of players) {
-      if (player['@_isUsed'] !== 'true') { continue }
-
-      state.players.push({
-        name: player['#text'],
-        isUsed: player['@_isUsed'] === 'true',
-        isAdmin: player['@_isAdmin'] === 'true',
-        uptime: parseInt(player['@_uptime'], 10),
-        x: parseFloat(player['@_x']),
-        y: parseFloat(player['@_y']),
-        z: parseFloat(player['@_z'])
+    try {
+      const request = await this.request({
+        url: `http://${this.options.host}:${this.options.port}/feed/dedicated-server-stats.xml?code=${this.options.token}`,
+        responseType: 'text'
       })
-    }
 
-    state.raw = {
-      data: request,
-      mods: []
-    }
+      const isValidXML = XMLValidator.validate(request)
+      if (!isValidXML) {
+        throw new Error('Invalid XML received from Farming Simulator Server')
+      }
 
-    const mods = serverInfo?.Mods?.Mod || [];
+      const parser = new XMLParser({ ignoreAttributes: false })
+      const parsed = parser.parse(request)
 
-    for (const mod of mods) {
-      if (mod['@_name'] == null) { continue }
+      const serverInfo = parsed.Server
+      const playerInfo = serverInfo.Slots
 
-      state.raw.mods.push({
-        name: mod['#text'],
-        short_name: mod['@_name'],
-        author: mod['@_author'],
-        version: mod['@_version'],
-        hash: mod['@_hash']
+      const decodeEntities = str => str
+        ? str
+            .replace(/&#(\d+);/g, (_, dec) => String.fromCharCode(dec))
+            .replace(/&lt;/g, '<')
+            .replace(/&gt;/g, '>')
+            .replace(/&amp;/g, '&')
+            .replace(/&quot;/g, '"')
+            .replace(/&apos;/g, "'")
+            .replace(/&#94;/g, '^')
+        : str
+
+      state.name = decodeEntities(serverInfo['@_name'])
+      state.map = decodeEntities(serverInfo['@_mapName'])
+      state.version = serverInfo['@_version']
+
+      const numPlayers = parseInt(playerInfo['@_numUsed'], 10) || 0
+      const maxPlayers = parseInt(playerInfo['@_capacity'], 10) || 0
+
+      state.numplayers = numPlayers
+      state.maxplayers = maxPlayers
+      const players = playerInfo.Player || []
+      const vehicles = serverInfo?.Vehicles?.Vehicle || []
+
+      vehicles.forEach(v => {
+        v['@_controller'] = decodeEntities(v['@_controller'])
+        v['@_name'] = decodeEntities(v['@_name'])
       })
-    }
 
-    state.version = serverInfo['@_version']
+      state.players = []
+      let idCounter = 0
+
+      for (const player of players) {
+        if (player['@_isUsed'] !== 'true') continue
+
+        const playerName = decodeEntities(player['#text'])
+        let x = null, z = null
+        let machineName = 'brak'
+
+        const vehicle = vehicles.find(v => v['@_controller'] === playerName)
+        if (vehicle) {
+          machineName = vehicle['@_name'] || 'brak'
+          x = parseFloat(vehicle['@_x']) || null
+          z = parseFloat(vehicle['@_z']) || null
+        } else {
+          x = parseFloat(player['@_x']) || null
+          z = parseFloat(player['@_z']) || null
+        }
+
+        state.players.push({
+          id: idCounter++,
+          name: playerName,
+          isAdmin: player['@_isAdmin'] === 'true',
+          machine_name: machineName,
+          x,
+          z
+        })
+      }
+
+      state.online = true
+    } catch (err) {
+      console.error(
+        'Błąd pobierania danych z serwera:',
+        err,
+        'Zawartość: ',
+        err?.response || '',
+        'Dane: ',
+        `http://${this.options.host}:${this.options.port}/feed/dedicated-server-stats.xml?code=${this.options.token}`
+      )
+
+      state.online = false
+      state.players = []
+    }
   }
 }
